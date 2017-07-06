c8f5771380679d07e88f02e636edf0807b103ed6
Adam King <adam81king@gmail.com>
2017-07-06 15:09:29 -0400

    Added FizzBuzz

--- LoopsPractice/Program.cs
+++ LoopsPractice/Program.cs
@@ -215,19 +215,47 @@ namespace LoopsPractice
             //Outer loop will go line by line
             //The inner loop will go over different elements in the line.
             //Result:
-            //   1
-            //  1 2
-            // 1 2 3 
+            //1
+            //1 2
+            //1 2 3 
             //1 2 3 4...
-            
-            for(int rows =1; rows <=4; rows++)
+
+            //for(int rows =1; rows <=4; rows++)
+            //{
+            //    for(int columns = 1; columns <= rows; columns++)
+            //    {
+            //        Console.Write(columns);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            Console.WriteLine("Lets play FizzBuzz!");
+            Console.WriteLine("Please enter your FIZZ number");
+            int fizz = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your BUZZ number");
+            int buzz = int.Parse(Console.ReadLine());
+            Console.WriteLine("What number would you like me to count to? (Pick a high one)");
+            int endNumber = int.Parse(Console.ReadLine());
+            for(int i = 0; i <= endNumber; i++)
             {
-                for(int columns = 1; columns <= rows; columns++)
+                if(i % fizz == 0 && i % buzz == 0)
                 {
-                    Console.Write(columns);
+                    Console.WriteLine("Fizz Buzz!");
                 }
-                Console.WriteLine();
-            }
+                else if (i % fizz == 0)
+                {
+                    Console.WriteLine("Fizz!");
+                }
+
+            else if (i % buzz == 0)
+                {
+                    Console.WriteLine("Buzz!");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             
               
 
