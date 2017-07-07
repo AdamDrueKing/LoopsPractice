ba72282ecad84aee155c318b79031739b1e51223
Adam King <adam81king@gmail.com>
2017-07-07 10:51:49 -0400

    Worked on favorite movies problem

--- LoopsPractice/Program.cs
+++ LoopsPractice/Program.cs
@@ -275,7 +275,35 @@ namespace LoopsPractice
             //Create an array of your favorite movies.Print out each movie in the list, unless the move starts with a vowel. 
             //Hint: Research.startsWith()
 
+            string[] faveMovies = { "Star Wars", "Anchorman", "Avatar", "Serenity", "Super Troopers" };
+            foreach(string movie in faveMovies)
+            {
+                if (movie.StartsWith("A"))
+                {
+                    continue;
+                }
+                else if (movie.StartsWith("E"))
+                {
+                    continue;
+                }
+                else if (movie.StartsWith("I"))
+                {
+                    continue;
+                }
+                else if (movie.StartsWith("O"))
+                {
+                    continue;
+                }
+                else if (movie.StartsWith("U"))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine(movie);
+                }
 
+            }
 
 
 
